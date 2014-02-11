cedabc8d0bf5156523d8d7049291bb0919f08bb6
Ben Langholz <Ben@Bens-iMac.local>
2014-02-10 20:49:43 -0800

    Initial Commit

---
+++ tipcalculatorTests/tipcalculatorTests.h
@@ -0,0 +1,13 @@
+//
+//  tipcalculatorTests.h
+//  tipcalculatorTests
+//
+//  Created by Ben Langholz on 2/10/14.
+//  Copyright (c) 2014 Ben Langholz. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface tipcalculatorTests : SenTestCase
+
+@end
---
+++ tipcalculatorTests/tipcalculatorTests.m
@@ -0,0 +1,32 @@
+//
+//  tipcalculatorTests.m
+//  tipcalculatorTests
+//
+//  Created by Ben Langholz on 2/10/14.
+//  Copyright (c) 2014 Ben Langholz. All rights reserved.
+//
+
+#import "tipcalculatorTests.h"
+
+@implementation tipcalculatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in tipcalculatorTests");
+}
+
+@end
