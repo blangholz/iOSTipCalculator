11fd9c2bb6c35adcd1f0c693d5ea439422b0b78d
Ben Langholz <blangholz@fb.com>
2014-02-12 14:58:53 -0800

    firsts commit

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Ben Langholz on 2/10/14.
+//  Copyright (c) 2014 Ben Langholz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
